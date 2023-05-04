4e483ebe8229a7287ce3f26ea465f77ed9e249cb
Sany-SHiriCE <Sany-SHiriCE@302D-10>
2023-05-04 15:45:41 +0700

    Добавьте файлы проекта.

---
+++ OperationOverloadNew/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OperationOverloadNew//Лабораторная работа по Мищенко №8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Time t1 = new Time(1, 30, 0);
+            Time t2 = new Time(2, 45, 15);
+
+            Console.WriteLine($"Первое время - t1 = {t1}");
+            Console.WriteLine($"Второе время - t2 = {t2}");
+
+            Time sum = t1 + t2;
+            Console.WriteLine($"Суммая времени t1 + t2 = {sum}");
+
+            Time diff = t2 - t1;
+            Console.WriteLine($"Разница времени t2 - t1 = {diff}");
+
+            Console.WriteLine($"Различные операции с временем: ");
+
+            Console.WriteLine($"t1 == t2? {t1 == t2}");
+            Console.WriteLine($"t1 != t2? {t1 != t2}");
+            Console.WriteLine($"t1 < t2? {t1 < t2}");
+            Console.WriteLine($"t1 > t2? {t1 > t2}");
+            Console.WriteLine($"t1 <= t2? {t1 <= t2}");
+            Console.WriteLine($"t1 >= t2? {t1 >= t2}");
+
+            Console.WriteLine($"t1++ = {t1++}");
+            Console.WriteLine($"++t1 = {++t1}");
+            Console.WriteLine($"t2-- = {t2--}");
+            Console.WriteLine($"--t2 = {--t2}");
+        }
+    }
+
+}
---
+++ OperationOverloadNew/Time.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperationOverloadNew
+{
+    public class Time
+    {
+        private int hour;
+        private int minute;
+        private int second;
+
+        public Time(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        public override string ToString()
+        {
+            return $"{hour:D2}:{minute:D2}:{second:D2}";
+        }
+
+        public static Time operator +(Time t1, Time t2)
+        {
+            int totalSeconds = t1.second + t2.second + (t1.minute + t2.minute) * 60 + (t1.hour + t2.hour) * 3600;
+            int newHour = totalSeconds / 3600;
+            int newMinute = (totalSeconds % 3600) / 60;
+            int newSecond = totalSeconds % 60;
+            return new Time(newHour, newMinute, newSecond);
+        }
+
+        public static Time operator -(Time t1, Time t2)
+        {
+            int totalSeconds = t1.second - t2.second + (t1.minute - t2.minute) * 60 + (t1.hour - t2.hour) * 3600;
+            int newHour = totalSeconds / 3600;
+            int newMinute = (totalSeconds % 3600) / 60;
+            int newSecond = totalSeconds % 60;
+            return new Time(newHour, newMinute, newSecond);
+        }
+
+        public static bool operator ==(Time t1, Time t2)
+        {
+            return t1.hour == t2.hour && t1.minute == t2.minute && t1.second == t2.second;
+        }
+
+        public static bool operator !=(Time t1, Time t2)
+        {
+            return !(t1 == t2);
+        }
+
+        public static bool operator <(Time t1, Time t2)
+        {
+            return t1.hour < t2.hour || (t1.hour == t2.hour && (t1.minute < t2.minute || (t1.minute == t2.minute && t1.second < t2.second)));
+        }
+
+        public static bool operator >(Time t1, Time t2)
+        {
+            return t1.hour > t2.hour || (t1.hour == t2.hour && (t1.minute > t2.minute || (t1.minute == t2.minute && t1.second > t2.second)));
+        }
+
+        public static bool operator <=(Time t1, Time t2)
+        {
+            return t1 < t2 || t1 == t2;
+        }
+
+        public static bool operator >=(Time t1, Time t2)
+        {
+            return t1 > t2 || t1 == t2;
+        }
+
+        public static Time operator ++(Time t)
+        {
+            t.second++;
+            if (t.second == 60)
+            {
+                t.second = 0;
+                t.minute++;
+                if (t.minute == 60)
+                {
+                    t.minute = 0;
+                    t.hour = (t.hour + 1) % 24;
+                }
+            }
+            return t;
+        }
+
+        public static Time operator --(Time t)
+        {
+            t.second--;
+            if (t.second < 0)
+            {
+                t.second = 59;
+                t.minute--;
+                if (t.minute < 0)
+                {
+                    t.minute = 59;
+                    t.hour = (t.hour - 1 + 24) % 24;
+                }
+            }
+            return t;
+        }
+    }
+}
